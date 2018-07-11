ab2c8da8aa89d4f287767dd44cbd3673f973d3f6
German Gorelkin <german.gorelkin@gmail.com>
2018-07-11 22:13:10 +0300

    add sqlQuery type

--- hw6_db_explorer/db_explorer.go
+++ hw6_db_explorer/db_explorer.go
@@ -16,9 +16,17 @@ import (
 //	Name string
 //	Value *sql.RawBytes
 //}
-//type DBColumn struct {
-//	sql.ColumnType
-//}
+type DBColumn struct {
+	Field string
+	Type string
+	Collation sql.NullString
+	Null string
+	Key string
+	Default sql.NullString
+	Extra string
+	Privileges string
+	Comment string
+}
 
 func RBtoString(val interface{}) string {
 	return string(*(val.(*sql.RawBytes)))
@@ -40,6 +48,46 @@ func RBtoNullString(val interface{}) sql.NullString {
 func RBtoInt(val interface{}) (int, error) {
 	return strconv.Atoi(string(*(val.(*sql.RawBytes))))
 }
+
+type sqlQuery string
+func (q sqlQuery) Select(col ...string)sqlQuery {
+	if len(col) == 0 {
+		return sqlQuery("SELECT * ")
+	}
+	return sqlQuery("SELECT " + strings.Join(col, ",") + " ")
+}
+func (q sqlQuery) From(table string) sqlQuery {
+	return sqlQuery(string(q) + " FROM " + table + " ")
+}
+func (q sqlQuery) WhereByPRIKey(columns []*DBColumn, val string) sqlQuery {
+	if val == ""{
+		return q
+	}
+	var key string
+	for _, c := range columns {
+		if c.Key == "PRI" {
+			key = c.Field
+			continue
+		}
+	}
+	if key == "" {
+		return q
+	}
+	return sqlQuery(string(q) + " WHERE " + key + "=" + val + " ")
+}
+func (q sqlQuery) Limit(limit string) sqlQuery {
+	if _, err := strconv.Atoi(limit); err != nil {
+		limit = "5"
+	}
+	return sqlQuery(string(q) + " LIMIT " + limit + " ")
+}
+func (q sqlQuery) Offset(offset string) sqlQuery {
+	if _, err := strconv.Atoi(offset); err != nil {
+		offset = "0"
+	}
+	return sqlQuery(string(q) + " OFFSET " + offset + " ")
+}
+
 
 type DBResponse struct {
 	Response map[string]interface{} `json:"response,omitempty"`
@@ -72,7 +120,7 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 	// all table
 	tables, err := mux.getTables()
 	if err != nil {
-		log.Print(err)
+		return mux, err
 	}
 
 	// GET / - список таблиц
@@ -80,7 +128,7 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		res := NewResponse()
 
-		if req.URL.Path != "/" || req.Method != http.MethodGet{
+		if req.URL.Path != "/" || req.Method != http.MethodGet {
 			w.WriteHeader(http.StatusNotFound)
 			res.Error = "unknown table"
 			b, err := res.Json()
@@ -107,14 +155,45 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 	// POST /table_name/{id}
 	// PUT  /table_name/{id}
 	for _, table := range tables {
-		route := "/"+ table+ "/"
+		columns, err := mux.getColumns(table)
+		if err != nil {
+			return mux, err
+		}
+		route := "/" + table + "/"
 		mux.HandleFunc(route, func(w http.ResponseWriter, req *http.Request) {
 			path := strings.Split(req.URL.Path, "/")
-			table := path[0]
-			if req.Method == http.MethodGet{
-				data := mux.query(table, "1", "0")
+			table := path[1]
+			var id string
+			if len(path)==3 {
+				id = path[2]
+			}
+
+			if req.Method == http.MethodGet {
+				limit := req.FormValue("limit")
+				offset := req.FormValue("offset")
+				var q sqlQuery
+				q = q.Select().
+					From(table).
+					WhereByPRIKey(columns, id).
+					Limit(limit).Offset(offset)
+				log.Println(q)
+				data, err := mux.query(q)
+				if err != nil {
+					log.Println(err)
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
 				res := NewResponse()
-				res.Response["records"] = data
+				if len(data)>0 {
+					if id == "" {
+						res.Response["records"] = data
+					} else {
+						res.Response["record"] = data[0]
+					}
+				} else{
+					res.Error = "record not found"
+					w.WriteHeader(http.StatusNotFound)
+				}
 				b, err := res.Json()
 				if err != nil {
 					log.Println(err)
@@ -123,46 +202,47 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 				}
 				w.Write(b)
 
-			} else if req.Method == http.MethodPost{
+			} else if req.Method == http.MethodPost {
 
-			} else if req.Method == http.MethodPut{
+			} else if req.Method == http.MethodPut {
 
 			}
 		})
 	}
 
-
 	return mux, nil
 }
 
 
-func (h *DBHandler) query(table, limit, offset string)[]map[string]interface{} {
-	q := "SELECT * FROM " + table + " LIMIT " + limit + " OFFSET " + offset + ";"
-	log.Println(q)
-	rows, err := h.db.Query(q)
+func (h *DBHandler) query(q sqlQuery)([]map[string]interface{}, error) {
+	var data []map[string]interface{}
+	//q := "SELECT * FROM " + table +
+	//	" LIMIT " + limit +
+	//	" OFFSET " + offset
+	//log.Println(q)
+	rows, err := h.db.Query(string(q))
 	if err != nil {
-		panic(err)
+		return data, err
 	}
 
 	cols, err := rows.Columns()
 	if err != nil {
-		panic(err)
+		return data, err
 	}
 	colType, err := rows.ColumnTypes()
 	if err != nil {
-		panic(err)
+		return data, err
 	}
 
 	vals := make([]interface{}, len(cols))
 	for i := range cols {
 		vals[i] = new(sql.RawBytes)
 	}
-	var data []map[string]interface{}
 
 	for rows.Next() {
 		err = rows.Scan(vals...)
 		if err != nil {
-			panic(err)
+			return data, err
 		}
 
 		val := make(map[string]interface{}, len(cols))
@@ -192,10 +272,10 @@ func (h *DBHandler) query(table, limit, offset string)[]map[string]interface{} {
 	}
 	rows.Close()
 	if err := rows.Err(); err != nil {
-		panic(err)
+		return data, err
 	}
 
-	return data
+	return data, err
 }
 
 func (h *DBHandler) getTables() ([]string, error) {
@@ -220,4 +300,31 @@ func (h *DBHandler) getTables() ([]string, error) {
 	}
 
 	return tables, nil
+}
+
+func (h *DBHandler) getColumns(table string) ([]*DBColumn, error) {
+	rows, err := h.db.Query("SHOW FULL COLUMNS FROM `"+table+"`;")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var columns []*DBColumn
+
+	for rows.Next() {
+		col := &DBColumn{}
+		err = rows.Scan(
+			&col.Field, &col.Type, &col.Collation,
+			&col.Null, &col.Key, &col.Default, &col.Extra,
+			&col.Privileges, &col.Comment)
+		if err != nil {
+			return nil, err
+		}
+		columns = append(columns, col)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return columns, nil
 }
